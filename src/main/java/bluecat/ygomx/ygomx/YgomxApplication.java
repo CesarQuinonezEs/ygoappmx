86401448061e8bab32614065b984a1051f3dbeee
CesarQuinonezEs <quinonez.cesar@uabc.edu.mx>
2024-06-17 10:56:26 -0700

    frist

---
+++ src/main/java/bluecat/ygomx/ygomx/YgomxApplication.java
@@ -0,0 +1,13 @@
+package bluecat.ygomx.ygomx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgomxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgomxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bluecat/ygomx/ygomx/YgomxApplicationTests.java
@@ -0,0 +1,13 @@
+package bluecat.ygomx.ygomx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgomxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
